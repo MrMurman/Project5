6cbc31651a9b988761d7590c1f4aa0f8ce805574
MrMurman <storm2150@yandex.ru>
2021-06-28 17:38:34 +0300

    Initial Commit

---
+++ Project5/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project5
+//
+//  Created by Андрей Бородкин on 28.06.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
